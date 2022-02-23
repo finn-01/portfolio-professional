9aa4218fcbc1e777f5e177717db93e11c6be8e6f
finn-01 <nguyensam576@gmail.com>
2022-02-24 04:08:30 +0700

    sanity config

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -3,31 +3,18 @@ import { motion } from "framer-motion";
 
 import "./About.scss";
 import { images } from "../../constants";
-
-const abouts = [
-	{
-		title: "Web Developer",
-		description: "I am good web developer.",
-		imgUrl: images.about01,
-	},
-	{
-		title: "Web Design",
-		description: "I am good web developer.",
-		imgUrl: images.about02,
-	},
-	{
-		title: "Web Animations",
-		description: "I am good web developer.",
-		imgUrl: images.about03,
-	},
-	{
-		title: "UI/UX",
-		description: "I am good web developer.",
-		imgUrl: images.about04,
-	},
-];
+import { urlFor, client } from "../../client";
 
 const About = () => {
+	const [abouts, setAbouts] = useState([]);
+
+	useEffect(() => {
+		const query = '*[_type == "abouts"]';
+
+		client.fetch(query).then((data) => {
+			setAbouts(data);
+		});
+	}, []);
 	return (
 		<>
 			<h2 className="head-text">
@@ -47,7 +34,7 @@ const About = () => {
 						className="app__profile-item"
 						key={about.title + index}
 					>
-						<img src={about.imgUrl} alt={about.title} />
+						<img src={urlFor(about.imgUrl)} alt={about.title} />
 						<h2 className="bold-text" style={{ marginTop: 20 }}>
 							{about.title}
 						</h2>
